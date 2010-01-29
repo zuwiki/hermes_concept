b1c1854db3d65c7611cd8b6254659fb63aa7de62
zuwiki <zuwiki@gmail.com>
2010-01-29 08:22:49 -0800

    Moving page-script ownership  tests to test_page.py and removing test_associations.py

--- tests/test_page.py
+++ tests/test_page.py
@@ -1,4 +1,4 @@
-from components import Page
+from components import Page, Script
 
 class test_EmptyPage():
 
@@ -21,4 +21,18 @@ class test_Page():
         self.page.content = self.page.content.replace('$id[test]', '$id[awesome]')
         
         assert self.page.content == mutated_content
+
+
+class test_Page_Script_ownership():
+
+    def setUp(self):
+        self.page = Page('I am a test page! How about that?', 
+                         Script('print("This is a test script.")'))
+
+    def test_page_should_have_script(self):
+        assert self.page.script.body == 'print("This is a test script.")'
+
+    def test_page_should_not_require_script_at_initialization(self):
+        assert Page('Only a body').script.body == Script().body
+
 
